c1e27f4e83f620f1e03e23394ec16b113c510fca
Mike <gostev@ebi.ac.uk>
2011-08-10 23:08:50 +0100

    WellDefinedObject

--- src/uk/ac/ebi/sampletab/WellDefinedObject.java
+++ src/uk/ac/ebi/sampletab/WellDefinedObject.java
@@ -1,8 +1,10 @@
 package uk.ac.ebi.sampletab;
 
+
 public class WellDefinedObject extends AnnotatedObject
 {
  private String className;
+// private Map<String, Attribute>
  
  public WellDefinedObject( String cls ) 
  {
@@ -10,5 +12,18 @@ public class WellDefinedObject extends AnnotatedObject
   
   if( ! Names.object2Properties.containsKey(cls) )
    throw new STParseException("Invalid class name");
+ }
+ 
+ public void setAttribute( Attribute attr )
+ {
+  if( ! className.equals( Names.propertyToObject.get(attr.getName()) ) )
+   throw new STParseException("Invalid property '"+attr.getName()+"' for class '"+className+"'");
+
+  super.addAnnotation(attr.getName(), attr);
+ }
+ 
+ public Attribute getAttribute( String atName )
+ {
+  return (Attribute)super.getAnnotation(atName);
  }
 }
